92b904179dd854bb3b2055ebf7ef3d8162036b1c
Oatmeal <oaticus@outlook.com>
2021-03-25 18:27:35 -0500

    win: Merged win32_window_handle_position with win32_window_handle_properties

--- src/win/include/private/dt/win/win32/window_handle.hh
+++ src/win/include/private/dt/win/win32/window_handle.hh
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <dt/win/win32/window_handle/position.hh>
 #include <dt/win/win32/window_handle/properties.hh>
 
 #include <dt/win/error.hh>
@@ -12,7 +11,6 @@
 namespace dt {
 
 class win32_window_handle final {
-  win32_window_handle_position   const _window_position;
   win32_window_handle_properties const _window_properties;
   std::wstring                   const _window_title;
   HWND                           const _window_handle;
--- src/win/include/private/dt/win/win32/window_handle/properties.hh
+++ src/win/include/private/dt/win/win32/window_handle/properties.hh
@@ -7,6 +7,10 @@ namespace dt {
 class win32_window_handle_properties final {
   DWORD  const _styles;
   DWORD  const _extended_styles;
+  int    const _width;
+  int    const _height;
+  int    const _x_coordinate;
+  int    const _y_coordinate;
   HWND   const _parent;
   HMENU  const _menu;
   LPVOID const _app_data;
@@ -14,6 +18,10 @@ class win32_window_handle_properties final {
 public:
   constexpr win32_window_handle_properties() noexcept
   : _styles(WS_OVERLAPPEDWINDOW),
+    _width(CW_USEDEFAULT),
+    _height(CW_USEDEFAULT),
+    _x_coordinate(CW_USEDEFAULT),
+    _y_coordinate(CW_USEDEFAULT),
     _extended_styles(0),
     _parent(nullptr),
     _menu(nullptr),
@@ -24,6 +32,10 @@ public:
   constexpr auto const& parent()         const noexcept { return _parent;          }
   constexpr auto const& menu()           const noexcept { return _menu;            }
   constexpr auto const& app_data()       const noexcept { return _app_data;        }
+  constexpr auto const& width()          const noexcept { return _width;           }
+  constexpr auto const& height()         const noexcept { return _height;          }
+  constexpr auto const& x_coordinate()   const noexcept { return _x_coordinate;    }
+  constexpr auto const& y_coordinate()   const noexcept { return _y_coordinate;    }
 };
 
 }
