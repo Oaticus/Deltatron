b462450fbedcf161f85cefe7ce0049b4d45e232f
Oatmeal <oaticus@outlook.com>
2021-03-29 20:44:33 -0500

    win: check for window creation failure

--- src/win/include/private/dt/win_imp.win32.hh
+++ src/win/include/private/dt/win_imp.win32.hh
@@ -33,7 +33,7 @@ public:
 
   ~win_imp() noexcept {}
 
-  void run() noexcept {
+  void run() {
     RegisterClassExW(&_window_class.window_class());
 
     _window_handle = _create_window();
@@ -45,10 +45,17 @@ public:
   }
 
 private:
-  HWND _create_window() const noexcept {
-    return CreateWindowExW(0, _window_class.window_class_name().c_str(), _window_name.c_str(), WS_OVERLAPPEDWINDOW,
+  HWND _create_window() const {
+    auto window_handle = HWND{};
+
+    window_handle = CreateWindowExW(0, _window_class.window_class_name().c_str(), _window_name.c_str(), WS_OVERLAPPEDWINDOW,
     CW_USEDEFAULT, CW_USEDEFAULT, _window_config.window_width(), _window_config.window_height(), nullptr, nullptr,
     _program_instance, nullptr);
+
+    if (window_handle == nullptr)
+      throw win_error("CreateWindowExW() failed with error code " + std::to_string(GetLastError()));
+
+    return window_handle;
   }
 
   HINSTANCE _get_program_instance() const {
